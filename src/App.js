bcc787c50a4690b5b1c16952ecceac28ee2549c8
Heph-zibah <oluwatosinhephzibah@gmail.com>
2022-11-27 14:58:36 +0100

    added the component path

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from '../src/Components/Navbar/Navbar';
+import Hero from '../src/Components/Hero/Hero';
+import Tour from '../src/Components/Tour/Tour';
+import Review from '../src/Components/Review/Review';
+import Faq from '../src/Components/FAQ/Faq';
+import Footer from '../src/Components/Footer/Footer';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<Navbar />
+			<Hero />
+			<Tour />
+			<Review />
+			<Faq />
+			<Footer />
+		</div>
+	);
 }
 
 export default App;
