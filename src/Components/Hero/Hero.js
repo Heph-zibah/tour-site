57edf7ec48b1e2c5d0447432b254c0991b506dbc
Heph-zibah <oluwatosinhephzibah@gmail.com>
2022-11-27 17:22:24 +0100

    update file

--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import '../Hero/Hero.css';
+import { GoBook } from 'react-icons/go';
 
 const Hero = () => {
-	return <div>Hero</div>;
+	return (
+		<>
+			<header>
+				<div className='container header__container'>
+					<h1>
+						live your <span>adventure</span>
+					</h1>
+					<p>
+						Don't wait until tomorrow. Discover your adventure now and feel the
+						sensation of closeness to nature around you.
+					</p>
+					<button>
+						<GoBook className='icon' /> book now
+					</button>
+				</div>
+			</header>
+		</>
+	);
 };
 
 export default Hero;
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,7 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
+import '../Navbar/Navbar.css';
+import { TiThMenuOutline } from 'react-icons/ti';
+import { FaTimes } from 'react-icons/fa';
 
 const Navbar = () => {
-	return <div>Navbar</div>;
+	const [nav, setNav] = useState(false);
+	return (
+		<>
+			<nav>
+				<div className='nav__container container'>
+					<h1 className='logo'>travel.</h1>
+					<div className='nav__link'>
+						<ul>
+							<li>Home</li>
+							<li>About</li>
+							<li>Tours</li>
+							<li>FAQs</li>
+							<li>Contact</li>
+						</ul>
+					</div>
+					<TiThMenuOutline className='nav__icon' />
+				</div>
+				<div className='mobile__link'>
+					<div className='mobile__header'>
+						<h1 className='logo'>travel.</h1>
+						<FaTimes className='nav__icon' />
+					</div>
+					<ul>
+						<li>Home</li>
+						<li>About</li>
+						<li>Tours</li>
+						<li>FAQs</li>
+						<li>Contact</li>
+					</ul>
+				</div>
+			</nav>
+		</>
+	);
 };
 
 export default Navbar;
