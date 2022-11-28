3b69fec3e7e578d786cbdc7feec814cb85a6ffb4
Heph-zibah <oluwatosinhephzibah@gmail.com>
2022-11-28 02:55:21 +0100

    changed component name from tour to tours

---
+++ src/Components/Tours/Tours.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import data from '../Tours/ToursData';
+import '../Tours/Tours.css';
+
+const Tours = () => {
+	return (
+		<>
+			<section></section>
+		</>
+	);
+};
+
+export default Tours;
---
+++ src/Components/Tours/ToursData.js
@@ -0,0 +1,20 @@
+import tour1 from '../../Assets/tour-1.jpg';
+import tour2 from '../../Assets/tour-2.jpg';
+import tour3 from '../../Assets/tour-3.jpg';
+import tour4 from '../../Assets/tour-4.jpg';
+import tour5 from '../../Assets/tour-5.jpg';
+import tour6 from '../../Assets/tour-6.jpg';
+const Tours = [
+	{
+		id: 1,
+		image: tour1,
+		title: 'Krabi, Thailand',
+		price: 20
+	},
+	{ id: 2, image: tour2, title: 'Multnomah Falls, United State', price: 80 },
+	{ id: 3, image: tour3, title: 'Chitwan National Park, Nepal', price: 120 },
+	{ id: 4, image: tour4, title: 'Mont Blanc, France', price: 200 },
+	{ id: 5, image: tour5, title: 'Erg Chebbi, Morocco', price: 60 },
+	{ id: 6, image: tour6, title: 'Adjara, Georgia', price: 78 }
+];
+export default Tours;
