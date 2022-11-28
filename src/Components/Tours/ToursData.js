89eafacecbf3a4cdd0ca145c57b393821f1b4d4b
Heph-zibah <oluwatosinhephzibah@gmail.com>
2022-11-28 08:40:15 +0100

    added the data

--- src/Components/Tours/ToursData.js
+++ src/Components/Tours/ToursData.js
@@ -4,17 +4,30 @@ import tour3 from '../../Assets/tour-3.jpg';
 import tour4 from '../../Assets/tour-4.jpg';
 import tour5 from '../../Assets/tour-5.jpg';
 import tour6 from '../../Assets/tour-6.jpg';
-const Tours = [
+const tours = [
 	{
 		id: 1,
-		image: tour1,
+		img: tour1,
+		rate: 4.5,
 		title: 'Krabi, Thailand',
 		price: 20
 	},
-	{ id: 2, image: tour2, title: 'Multnomah Falls, United State', price: 80 },
-	{ id: 3, image: tour3, title: 'Chitwan National Park, Nepal', price: 120 },
-	{ id: 4, image: tour4, title: 'Mont Blanc, France', price: 200 },
-	{ id: 5, image: tour5, title: 'Erg Chebbi, Morocco', price: 60 },
-	{ id: 6, image: tour6, title: 'Adjara, Georgia', price: 78 }
+	{
+		id: 2,
+		img: tour2,
+		rate: 4,
+		title: 'Multnomah Falls, United State',
+		price: 80
+	},
+	{
+		id: 3,
+		img: tour3,
+		rate: 4.3,
+		title: 'Chitwan National Park, Nepal',
+		price: 120
+	},
+	{ id: 4, img: tour4, rate: 4.1, title: 'Mont Blanc, France', price: 200 },
+	{ id: 5, img: tour5, rate: 4.2, title: 'Erg Chebbi, Morocco', price: 60 },
+	{ id: 6, img: tour6, rate: 4.4, title: 'Adjara, Georgia', price: 78 }
 ];
-export default Tours;
+export default tours;
