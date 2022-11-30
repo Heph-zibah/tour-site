889aaf219cc2c34725ed724d93c81c5becde9e3c
Heph-zibah <oluwatosinhephzibah@gmail.com>
2022-11-30 21:23:30 +0100

    update file

--- src/Components/FAQ/Faq.js
+++ src/Components/FAQ/Faq.js
@@ -1,7 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+import data from '../FAQ/FaqData';
+import '../FAQ/Faq.css';
+import SingleFaq from '../FAQ/SingleFaq';
+import { BsFillPatchQuestionFill } from 'react-icons/bs';
 
 const Faq = () => {
-	return <div>Faq</div>;
+	const [faqs, setFaqs] = useState(data);
+	return (
+		<>
+			<section className='container'>
+				<div className='faq__container'>
+					<h1>
+						<BsFillPatchQuestionFill />
+						frequently asked <span> questions</span>
+						<BsFillPatchQuestionFill />
+					</h1>
+					<div>
+						{faqs.map((faq) => {
+							return <SingleFaq key={faq.id} {...faq} />;
+						})}
+					</div>
+				</div>
+			</section>
+		</>
+	);
 };
 
 export default Faq;
