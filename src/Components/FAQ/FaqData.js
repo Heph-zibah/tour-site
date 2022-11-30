89df200a841813c89124343ade79d09ae6a04618
Heph-zibah <oluwatosinhephzibah@gmail.com>
2022-11-30 21:22:03 +0100

    added the data file

---
+++ src/Components/FAQ/FaqData.js
@@ -0,0 +1,34 @@
+const data = [
+	{
+		id: 1,
+		title: 'What are the most surreal places to visit?',
+		text:
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sem ex, lobortis vitae condimentum quis, pharetra at nisl. Quisque accumsan risus eu auctor pellentesque. Donec a lectus ullamcorper, tempor dolor non, tristique odio. Quisque velit nisl, malesuada eget porttitor porttitor, scelerisque ut nulla. Phasellus tincidunt justo vel nibh tempus, eu condimentum dui consequat. '
+	},
+	{
+		id: 2,
+		title:
+			'Why do you need to turn off all your electronic devices before an airplane takes off and lands?',
+		text:
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sem ex, lobortis vitae condimentum quis, pharetra at nisl. Quisque accumsan risus eu auctor pellentesque. Donec a lectus ullamcorper, tempor dolor non, tristique odio. Quisque velit nisl, malesuada eget porttitor porttitor, scelerisque ut nulla. Phasellus tincidunt justo vel nibh tempus, eu condimentum dui consequat. '
+	},
+	{
+		id: 3,
+		title: 'Do you have to be rich to travel the world?',
+		text:
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sem ex, lobortis vitae condimentum quis, pharetra at nisl. Quisque accumsan risus eu auctor pellentesque. Donec a lectus ullamcorper, tempor dolor non, tristique odio. Quisque velit nisl, malesuada eget porttitor porttitor, scelerisque ut nulla. Phasellus tincidunt justo vel nibh tempus, eu condimentum dui consequat. '
+	},
+	{
+		id: 4,
+		title: 'What’s it really like to be a travel writer?',
+		text:
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sem ex, lobortis vitae condimentum quis, pharetra at nisl. Quisque accumsan risus eu auctor pellentesque. Donec a lectus ullamcorper, tempor dolor non, tristique odio. Quisque velit nisl, malesuada eget porttitor porttitor, scelerisque ut nulla. Phasellus tincidunt justo vel nibh tempus, eu condimentum dui consequat. '
+	},
+	{
+		id: 5,
+		title: 'Where are the best places to travel alone?',
+		text:
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sem ex, lobortis vitae condimentum quis, pharetra at nisl. Quisque accumsan risus eu auctor pellentesque. Donec a lectus ullamcorper, tempor dolor non, tristique odio. Quisque velit nisl, malesuada eget porttitor porttitor, scelerisque ut nulla. Phasellus tincidunt justo vel nibh tempus, eu condimentum dui consequat. '
+	}
+];
+export default data;
